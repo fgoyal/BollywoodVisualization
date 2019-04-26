d139553f27b765b2927cb1e547d07f1ca779918c
Fgoyal <fgoyal2@illinois.edu>
2019-04-26 16:29:52 -0500

    Cleaned up columns to remove unnecessary ones and split by comma

--- data.py
+++ data.py
@@ -4,17 +4,27 @@ import matplotlib.pyplot as plt
 
 
 data = pd.read_csv('bollywood_kaggle.csv')
+
+# drop unneeded columns
 data = data.drop('Genre', axis=1)
 data = data.drop('Title', axis=1)
-cast = data.Cast.str.split(',', expand=True)
-data = data.iloc[::-1]
+
+# create new column with the directors and cast combined, properly formatted
+data['Director'] = data['Director'].str.replace(' ',',')
+data['People'] = data['Director'] + "," + data["Cast"]
+data['People'] = data['People'].str.replace('(','')
+data['People'] = data['People'].str.replace(')','')
+
+# drop unneeded columns
 data = data.drop('Cast', axis=1)
+data = data.drop('Director', axis=1)
+
+# split People column by comma
+data = data.join(data.People.str.split(',', expand=True))
+data = data.drop('People', axis=1)
+
+# read in descending order
+data = data.iloc[::-1]
+
+
 print(data.head())
-
-
-ax = data[['Year','Director']].plot(kind='bar', title ="Year v Director", figsize=(15, 10), legend=True, fontsize=12)
-ax.set_xlabel("Year", fontsize=12)
-ax.set_ylabel("Director", fontsize=12)
-plt.show()
-cast = cast.iloc[::-1]
-print(cast.head())
